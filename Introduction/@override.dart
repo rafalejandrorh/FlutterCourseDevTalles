5a058cf65dc68741e7928c623cf893ae0edf6371
Rafael Rivero <rafalejandrorivero@gmail.com>
2024-08-03 16:21:20 -0400

    Add Classes and @override

---
+++ Introduction/@override.dart
@@ -0,0 +1,27 @@
+
+
+void main() {
+  final wolverine = new Hero(name: 'Logan', power: 'Regeneration');
+  //final wolverine = new Hero(name: 'Logan');
+  
+  print(wolverine);
+  print('Name: ${wolverine.name}');
+  print('Power: ${wolverine.power}');
+}
+
+class Hero {
+  
+  String name;
+  String power;
+  
+  Hero({
+    required this.name, 
+    this.power = 'Within Power'
+  }); 
+  
+  @override
+  String toString() {
+    return '$name - $power';
+  }
+  
+}
---
+++ Introduction/classes.dart
@@ -0,0 +1,29 @@
+
+
+void main() {
+  final wolverine = new Hero('Logan', 'Regeneration');
+  
+  print(wolverine);
+  print('Name: ${wolverine.name}');
+  print('Power: ${wolverine.power}');
+}
+
+class Hero {
+  
+  String name;
+  String power;
+  
+  // Forma N°2:
+  Hero(this.name, this.power); 
+  
+  // Forma N°1:
+  //Hero(String pName, String pPower)
+  //  : name = pName,
+  //    power = pPower;
+  
+  // No se inicializa las variables y da error
+  // {
+  //this.name = pName;
+  //this.power = pPower;
+  // }
+}
