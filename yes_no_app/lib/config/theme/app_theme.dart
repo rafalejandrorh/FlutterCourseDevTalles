0402967f083976392306fe9e8d8d487fa33cdc1c
Rafael Rivero <rafael.rivero@simpledatacorp.com>
2024-10-05 23:07:33 -0400

    Add Yes No App

---
+++ yes_no_app/lib/config/theme/app_theme.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+const Color _customColor = Color(0xFF49149F);
+const List<Color> _colorThemes = [
+  _customColor,
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.yellow,
+  Colors.orange,
+  Colors.pink
+];
+
+class AppTheme {
+
+  final int selectedColor;
+
+  AppTheme({
+    required this.selectedColor
+  }): assert(selectedColor >= 0 && selectedColor < _colorThemes.length - 1, 'Colors must be between 0 and ${_colorThemes.length}');
+
+  ThemeData theme() {
+    return ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: _colorThemes[selectedColor]
+    );
+  }
+
+}
